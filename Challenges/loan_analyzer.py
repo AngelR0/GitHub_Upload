aa54b409b30a8d3fa0f5ed0d5879cbf2d9c87c12
AngelR0 <reyesangel216321@gmail.com>
2022-01-06 17:47:46 -0600

    Created a present value formula function.

--- Challenges/loan_analyzer.py
+++ Challenges/loan_analyzer.py
@@ -68,10 +68,21 @@ new_loan = {
 # Define a new function that will be used to calculate present value.
 
 
-# @TODO: Use the function to calculate the present value of the new loan given below.
-#    Use an `annual_discount_rate` of 0.2 for this new loan calculation.
-# YOUR CODE HERE!
-print(f"The present value of the loan is: {present_value}")
+def calculate_present_value(future_value, remaining_months, annual_discount_rate):
+    present_value = future_value / \
+        (1 + annual_discount_rate / 12) ** remaining_months
+    return present_value
+
+
+#   Calculate the present value of the new loan given below.
+annual_discount_rate = .20
+present_value = calculate_present_value(
+    new_loan["future_value"],
+    new_loan["remaining_months"],
+    annual_discount_rate
+)
+
+print(f"The present value of the loan is: {present_value:.02f}\n")
 
 
 """Part 4: Conditionally filter lists of loans.
